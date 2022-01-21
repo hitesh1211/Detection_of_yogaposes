ffd4c590106b19d670b355a76f905b1ff3624368
HITESH <61309690+hitesh1211@users.noreply.github.com>
2022-01-21 10:45:46 +0530

    Add files via upload

---
+++ Home.js
@@ -0,0 +1,45 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+import './Home.css'
+
+export default function Home() {
+
+    return ( <
+        div className = 'home-container' >
+        <
+        div className = 'home-header' >
+        <
+        h1 className = 'home-heading' > Yoga < /h1> <
+        Link to = '/about' >
+        <
+        button className = "btn btn-secondary"
+        id = "about-btn" >
+        About <
+        /button> < /
+        Link > <
+        /div>
+
+        <
+        h1 className = "description" > Yoga Pose Detection < /h1> <
+        div className = "home-main" >
+        <
+        div className = "btn-section" >
+        <
+        Link to = '/start' >
+        <
+        button className = "btn start-btn" >
+        Begin the session < /button> < /
+        Link > <
+        Link to = '/tutorials' >
+        <
+        button className = "btn start-btn" >
+        Tutorials < /button> < /
+        Link >
+
+        <
+        /div> < /
+        div > <
+        /div>
+    )
+}
